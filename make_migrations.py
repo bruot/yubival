f594d6c4bf7b43215a65f8d472e49eb6aa055e85
Nicolas Bruot <coremeltdown@bruot.org>
2021-10-31 12:02:34 +0100

    Pass arguments from make_migrations.py to the manage.py command
    This allows things like
    
      poetry run python make_migrations.py -n MIGRATION_NAME

--- make_migrations.py
+++ make_migrations.py
@@ -4,5 +4,5 @@ import sys
 if __name__ == '__main__':
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tests.settings')
     from django.core.management import execute_from_command_line
-    args = sys.argv + ['makemigrations', 'yubival']
+    args = [__file__, 'makemigrations', 'yubival'] + sys.argv[1:]
     execute_from_command_line(args)
