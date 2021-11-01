ad01d072ce040b5c4ab2e1ef40a484c2e02eff02
Nicolas Bruot <coremeltdown@bruot.org>
2021-11-01 12:19:05 +0100

    Add function to generate argparse "data types" from Django model fields

--- tests/test_validators.py
+++ tests/test_validators.py
@@ -1,7 +1,9 @@
 from django.core.exceptions import ValidationError
+from django.core.validators import MaxValueValidator
+from django.db.models import FloatField, IntegerField
 from django.test import TestCase
 
-from yubival.validators import validate_hex, validate_modhex, LengthValidator
+from yubival.validators import validate_hex, validate_modhex, LengthValidator, argparse_type
 
 
 class TestLengthValidator(TestCase):
@@ -84,3 +86,55 @@ class TestValidateModHex(TestCase):
         # THEN
         with self.assertRaises(ValidationError):
             validate_modhex(value)
+
+
+class TestArgparseType(TestCase):
+    def test_function_returns_ouptput_data_type(self):
+        # GIVEN
+        field = FloatField()
+        type_func = argparse_type(field)
+
+        # WHEN
+        new_value = type_func('123')
+
+        # THEN
+        self.assertTrue(type(new_value) is float)
+
+    def test_function_returns_value(self):
+        # GIVEN
+        field = FloatField()
+        type_func = argparse_type(field)
+
+        # WHEN
+        new_value = type_func('123')
+
+        # THEN
+        self.assertEqual(123.0, new_value)
+
+    def test_function_has_custom_name(self):
+        # GIVEN
+        field = FloatField()
+
+        # WHEN
+        type_func = argparse_type(field, 'custom')
+
+        # THEN
+        self.assertEqual('custom', type_func.__name__)
+
+    def test_nonconvertible_input_raises_exception(self):
+        # GIVEN
+        field = IntegerField()
+        type_func = argparse_type(field)
+
+        # THEN
+        with self.assertRaises(ValueError):
+            type_func('abc')
+
+    def test_function_raises_valueerror_when_validator_fails(self):
+        # GIVEN
+        field = IntegerField(validators=[MaxValueValidator(10)])
+        type_func = argparse_type(field)
+
+        # THEN
+        with self.assertRaises(ValueError):
+            type_func(20)
